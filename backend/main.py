7309554fd580b4090267407d2006c2688c07b6a8
billel mokeddem <bilel.crysis3@gmail.com>
2021-03-01 21:12:30 +0100

    Splitting into sentences by dot and colon

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,11 @@ file = file.replace(" ``/``", '') \
     .replace("''/'' ",'') \
     .replace('(/( ','') \
     .replace(')/)','')
+
+# Splitting into sentences by '.' (sby <==> split by)
+sents_sby_dot = file.split('./.')
+
+# Splitting by colon
+sents_sby_dot_colon = []
+for sent in sents_sby_dot:
+    sents_sby_dot_colon += sent.split(':/:')
